269d5e65a9bbaf5d7bca95f2a581bff5c4ee07ca
JordiDev <alexandernovas.com@gmail.com>
2026-06-07 12:03:35 -0400

    Prueba unitaria y Alerta Yaml

---
+++ test_hola.py
@@ -0,0 +1,10 @@
+import unittest
+from hola import saludo
+
+class TestHola(unittest.TestCase):
+
+    def test_saludo(self):
+        self.assertEqual(saludo(), "Hola Mundo")
+
+if __name__ == "__main__":
+    unittest.main()
